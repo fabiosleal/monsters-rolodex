c394248e4b6896e40fb2ada611fee3aaf588532d
Fabio Leal <fabioleal@Fabios-Mac-mini.local>
2020-04-28 01:50:19 -0300

    Course - chapter 3 - lesson 24

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state; //destructuring
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(e) => {
+            this.setState({ searchField: e.target.value });
+          }}
+        />
+        {/* <CardList monsters={this.state.monsters} /> */}
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// import React, { Component } from "react";
+// import "./App.css";
+
+// class App extends Component {
+//     constructor() {
+//         super();
+
+//         this.state = {
+//             mosnters: [
+//                 {
+//                     id: "jskl",
+//                     name: "Frankenstein"
+//                 },
+//                 {
+//                     id: "hasd",
+//                     name: "Wolfman"
+//                 },
+//                 {
+//                     id: "sadl",
+//                     name: "Mummy"
+//                 },
+//                 {
+//                     id: "adsh",
+//                     name: "Dracula"
+//                 }
+//             ]
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div className='App'>
+//                 {this.state.mosnters.map(monster => (
+//                     <h1 key={monster.id}>{monster.name}</h1>
+//                 ))}
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
